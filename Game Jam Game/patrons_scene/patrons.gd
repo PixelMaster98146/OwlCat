0150d6952f15961380533b6494003d9ac47b2682
PixelMaster98146 <samirc8976@gmail.com>
2024-07-31 19:01:24 +1000

    Add files via upload

--- Game Jam Game/patrons_scene/patrons.gd
+++ Game Jam Game/patrons_scene/patrons.gd
@@ -21,7 +21,7 @@ var real_patron = true
 @onready var mean_text = ["@#$%", "Why can't you just do your job", "My day is ruined"]
 @onready var nice_text = ["Thanks", ":)", "Woohoo"]
 @onready var raidP1_text = ["You're a fool. ", "You've been decieved. ", "Idiot. "]
-@onready var raidP2_text = ["I'll be back later with friends!", "You're busted now!", "Adventurers rule!"]
+##@onready var raidP2_text = ["I'll be back later with friends!", "You're busted now!", "Adventurers rule!"]
 
 @onready var path1 = %PathFollow3D1
 @onready var path2 = %PathFollow3D2
@@ -37,7 +37,6 @@ var real_patron = true
 var id_card
 @onready var idcheck = $"../IDcheck"
 var mainscene
-@onready var GobAnim = $"Models/goblin animated/AnimationPlayer"
 
 @onready var mixarea = $"../Node3D"
 
@@ -153,6 +152,16 @@ func path():
 	var _customer8 = customer.duplicate()
 	var _customer9 = customer.duplicate()
 	var _customer10 = customer.duplicate()
+	var GobAnim1 = _customer1.get_child(1)
+	var GobAnim2 = _customer2.get_child(1)
+	var GobAnim3 = _customer3.get_child(1)
+	var GobAnim4 = _customer4.get_child(1)
+	var GobAnim5 = _customer5.get_child(1)
+	var GobAnim6 = _customer6.get_child(1)
+	var GobAnim7 = _customer7.get_child(1)
+	var GobAnim8 = _customer8.get_child(1)
+	var GobAnim9 = _customer9.get_child(1)
+	var GobAnim10 = _customer10.get_child(1)
 	$IDCard.get_random_info()
 	var _idcard = $IDCard.duplicate()
 	if path1.get_child_count() == 0: #and line_wait == false:
@@ -160,43 +169,53 @@ func path():
 		path1.add_child(_customer1)
 		_customer1.add_child(_idcard)
 		path1.progress = 0
+		GobAnim1.play("walk cycle")
 	elif path1.get_child_count() == 1 and path2.get_child_count() == 0:
 		#print("2")
 		path2.add_child(_customer2)
 		_customer2.add_child(_idcard)
 		path2.progress = 0
+		GobAnim2.play("walk cycle")
 	elif path2.get_child_count() == 1 and path3.get_child_count() == 0:
 		#print("3")
 		path3.add_child(_customer3)
 		path3.progress = 0
+		GobAnim3.play("walk cycle")
 	elif path3.get_child_count() == 1 and path4.get_child_count() == 0:
 		print("4")
 		path4.add_child(_customer4)
 		path4.progress = 0
+		GobAnim4.play("walk cycle")
 	elif path4.get_child_count() == 1 and path5.get_child_count() == 0:
 		print("5")
 		path5.add_child(_customer5)
 		path5.progress = 0
+		GobAnim5.play("walk cycle")
 	elif path5.get_child_count() == 1 and path6.get_child_count() == 0:
 		print("6")
 		path6.add_child(_customer6)
 		path6.progress = 0
+		GobAnim6.play("walk cycle")
 	elif path6.get_child_count() == 1 and path7.get_child_count() == 0:
 		print("7")
 		path7.add_child(_customer7)
 		path7.progress = 0
+		GobAnim7.play("walk cycle")
 	elif path7.get_child_count() == 1 and path8.get_child_count() == 0:
 		print("8")
 		path8.add_child(_customer8)
 		path8.progress = 0
+		GobAnim8.play("walk cycle")
 	elif path8.get_child_count() == 1 and path9.get_child_count() == 0:
 		print("9")
 		path9.add_child(_customer9)
 		path9.progress = 0
+		GobAnim9.play("walk cycle")
 	elif path9.get_child_count() == 1 and path10.get_child_count() == 0:
 		print("10")
 		path10.add_child(_customer10)
 		path10.progress = 0
+		GobAnim10.play("walk cycle")
 	else:
 		print("pass")
 		$PatronTimer.stop()
@@ -208,7 +227,7 @@ func _on_patron_leave_body_entered(body):
 
 func _on_counter_body_entered(body):
 	patronorders = body.get_parent()
-	body.get_parent_node_3d().look_at(player.position)
+	body.get_parent_node_3d().look_at(-player.position)
 	id_card = body.get_parent().get_node("IDCard")
 	if id_card != null:
 		default_id_pos = id_card.position
@@ -219,6 +238,8 @@ func _on_counter_body_entered(body):
 	selected = true
 	var counter_stop = body.get_parent().get_parent()
 	var model = body.get_parent()
+	var IDHandover = model.get_child(1)
+	IDHandover.play("give")
 	if counter_stop == path1:
 		moving1 = false
 	if counter_stop == path2:
@@ -239,7 +260,7 @@ func _on_counter_body_entered(body):
 		moving9 = false
 	if counter_stop == path10:
 		moving10 = false
-	model.rotation_degrees += Vector3(0, 180, 0)
+	model.rotation_degrees += Vector3(0, 0, 0)
 	counter_full = true
 	##hand over id animation
 
@@ -247,6 +268,8 @@ func _on_line_up_1_body_entered(body):
 	var line1_stop = body.get_parent().get_parent()
 	var model = body.get_parent()
 	if counter_full == true:
+		var line_wait = model.get_child(1)
+		line_wait.stop()
 		if line1_stop == path1:
 			moving1 = false
 		if line1_stop == path2:
@@ -274,6 +297,8 @@ func _on_line_up_2_body_entered(body):
 	var line2_stop = body.get_parent().get_parent()
 	var model = body.get_parent()
 	if line1_full == true:
+		var line_wait = model.get_child(1)
+		line_wait.stop()
 		if line2_stop == path1:
 			moving1 = false
 		if line2_stop == path2:
@@ -301,6 +326,8 @@ func _on_line_up_3_body_entered(body):
 	var line3_stop = body.get_parent().get_parent()
 	var model = body.get_parent()
 	if line2_full == true:
+		var line_wait = model.get_child(1)
+		line_wait.stop()
 		if line3_stop == path1:
 			moving1 = false
 		if line3_stop == path2:
@@ -328,6 +355,8 @@ func _on_line_up_4_body_entered(body):
 	var line4_stop = body.get_parent().get_parent()
 	var model = body.get_parent()
 	if line3_full == true:
+		var line_wait = model.get_child(1)
+		line_wait.stop()
 		if line4_stop == path1:
 			moving1 = false
 		if line4_stop == path2:
@@ -355,6 +384,8 @@ func _on_line_up_5_body_entered(body):
 	var line5_stop = body.get_parent().get_parent()
 	var model = body.get_parent()
 	if line4_full == true:
+		var line_wait = model.get_child(1)
+		line_wait.stop()
 		if line5_stop == path1:
 			moving1 = false
 		if line5_stop == path2:
@@ -382,6 +413,8 @@ func _on_line_up_6_body_entered(body):
 	var line6_stop = body.get_parent().get_parent()
 	var model = body.get_parent()
 	if line5_full == true:
+		var line_wait = model.get_child(1)
+		line_wait.stop()
 		if line6_stop == path1:
 			moving1 = false
 		if line6_stop == path2:
@@ -409,6 +442,8 @@ func _on_line_up_7_body_entered(body):
 	var line7_stop = body.get_parent().get_parent()
 	var model = body.get_parent()
 	if line6_full == true:
+		var line_wait = model.get_child(1)
+		line_wait.stop()
 		if line7_stop == path1:
 			moving1 = false
 		if line7_stop == path2:
@@ -436,6 +471,8 @@ func _on_line_up_8_body_entered(body):
 	var line8_stop = body.get_parent().get_parent()
 	var model = body.get_parent()
 	if line7_full == true:
+		var line_wait = model.get_child(1)
+		line_wait.stop()
 		if line8_stop == path1:
 			moving1 = false
 		if line8_stop == path2:
@@ -463,6 +500,8 @@ func _on_line_up_9_body_entered(body):
 	var line9_stop = body.get_parent().get_parent()
 	var model = body.get_parent()
 	if line8_full == true:
+		var line_wait = model.get_child(1)
+		line_wait.stop()
 		if line9_stop == path1:
 			moving1 = false
 		if line9_stop == path2:
@@ -497,7 +536,10 @@ func _on_id_card_real():
 func _on_main_guess_id_fake():
 	if real_fake.back() == "real":
 		#print("denied, is real")
-		###Label#.text = mean_text.pick_random()
+		patronorders.get_node("patorder").show()
+		patronorders.get_node("patorder2").show()
+		patronorders.get_node("patorder").text = mean_text.pick_random()
+		patronorders.get_node("patorder2").text = ""
 		$Grumble.play()
 		await get_tree().create_timer(1).timeout
 		real_fake.pop_back()
@@ -513,7 +555,10 @@ func _on_main_guess_id_fake():
 		moving10 = true
 	elif real_fake.back() == "fake":
 		#print("denied, is fake")
-		###Label#.text = mean_text.pick_random()
+		patronorders.get_node("patorder").show()
+		patronorders.get_node("patorder2").show()
+		patronorders.get_node("patorder").text = mean_text.pick_random()
+		patronorders.get_node("patorder2").text = ""
 		$Grumble.play()
 		await get_tree().create_timer(1).timeout
 		real_fake.pop_back()
@@ -529,7 +574,9 @@ func _on_main_guess_id_fake():
 		moving10 = true
 
 func _on_main_guess_id_real():
-	ordering()
+	ordering()	
+	patronorders.get_node("patorder").show()
+	patronorders.get_node("patorder2").show()
 	updatelabel(patronorders, itemA,itemB)
 	if real_fake.back() == "real":
 		#print("accept, is real")
@@ -564,7 +611,7 @@ func _on_order_complete():
 	elif real_patron == false:
 		###Label#.text = str(raidP1_text.pick_random()) + str(raidP2_text.pick_random())
 		patronorders.get_node("patorder").text = str(raidP1_text.pick_random())
-		patronorders.get_node("patorder2").text = str(raidP2_text.pick_random())
+		#patronorders.get_node("patorder2").text = str(raidP2_text.pick_random())
 		raid.emit()##emit raid signal ##2 minutes later shop is forced to close from adventurer raid
 		$EvilLaugh.play()
 		move_speed = 10.0
@@ -625,10 +672,62 @@ func game_over_good():
 	##Dialouge
 	##Good ending screen
 
-
 func game_over_bad():
 	var corporat = $Models/Corporat.duplicate()
 	path1.add_child(corporat)
 	path1.progress = 0
 	##Dialouge
 	##Bad ending screen
+
+func _on_counter_body_exited(body):
+	var model = body.get_parent()
+	var resume_walk = model.get_child(1)
+	resume_walk.play("walk cycle")
+	model.rotation_degrees += Vector3(0, 50, 0)
+	patronorders.get_node("patorder").rotation_degrees += Vector3(0, 180, 0)
+	patronorders.get_node("patorder2").rotation_degrees += Vector3(0, 180, 0)
+
+func _on_line_up_1_body_exited(body):
+	var model = body.get_parent()
+	var resume_walk = model.get_child(1)
+	resume_walk.play("walk cycle")
+
+func _on_line_up_2_body_exited(body):
+	var model = body.get_parent()
+	var resume_walk = model.get_child(1)
+	resume_walk.play("walk cycle")
+
+func _on_line_up_3_body_exited(body):
+	var model = body.get_parent()
+	var resume_walk = model.get_child(1)
+	resume_walk.play("walk cycle")
+
+func _on_line_up_4_body_exited(body):
+	var model = body.get_parent()
+	var resume_walk = model.get_child(1)
+	resume_walk.play("walk cycle")
+
+func _on_line_up_5_body_exited(body):
+	var model = body.get_parent()
+	var resume_walk = model.get_child(1)
+	resume_walk.play("walk cycle")
+
+func _on_line_up_6_body_exited(body):
+	var model = body.get_parent()
+	var resume_walk = model.get_child(1)
+	resume_walk.play("walk cycle")
+
+func _on_line_up_7_body_exited(body):
+	var model = body.get_parent()
+	var resume_walk = model.get_child(1)
+	resume_walk.play("walk cycle")
+
+func _on_line_up_8_body_exited(body):
+	var model = body.get_parent()
+	var resume_walk = model.get_child(1)
+	resume_walk.play("walk cycle")
+
+func _on_line_up_9_body_exited(body):
+	var model = body.get_parent()
+	var resume_walk = model.get_child(1)
+	resume_walk.play("walk cycle")
